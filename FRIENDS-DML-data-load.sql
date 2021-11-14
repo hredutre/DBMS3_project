2963fdddba9d6f7b48853306706d2b9fb898b2de
hredutre <52985955+hredutre@users.noreply.github.com>
2021-11-14 14:03:51 +0600

    Add files via upload

---
+++ FRIENDS-DML-data-load.sql
@@ -0,0 +1,34 @@
+ .mode csv books
+ .separator ,
+ .import books.csv books
+
+
+
+ .mode csv user
+ .separator ,
+ .import user.csv user
+
+
+
+ .mode csv location
+ .separator ,
+ .import location.csv location
+
+
+
+ .mode csv rating
+ .separator ,
+ .import rating.csv rating
+
+
+ .mode csv login
+ .separator ,
+ .import login.csv login
+
+
+
+ .mode csv book_rec
+ .separator ,
+ .import book_rec.csv book_rec
+
+
